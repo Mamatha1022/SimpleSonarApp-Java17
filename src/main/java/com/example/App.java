dbddca2cb558eb5f836b296ec369d9b1bd088ca7
adhya2020 <97822101+adhya2020@users.noreply.github.com>
2025-06-07 11:53:14 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,8 @@ public class App {
     public int add(int a, int b) {
         return a + b;
     }
+
     public static void main(String[] args) {
-        System.out.println("SimpleSonarApp running");
+        System.out.println("SimpleSonarApp running on Java 17");
     }
 }
